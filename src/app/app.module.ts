77732c5a66208a8d3596f0c43cb68826a84cbb44
nipunsandaruwan <nipunsandaruwan47@gmail.com>
2022-06-18 11:45:55 +0530

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationbarComponent } from './navigationbar/navigationbar.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationbarComponent,
+    HeroSectionComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
